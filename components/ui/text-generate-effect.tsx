3150b1224c207b40982cca5dee44049c9aef2967
thatcatfromspace <encyclopedia748@gmail.com>
2024-03-03 02:09:29 +0530

    (NEW) text animations

---
+++ components/ui/text-generate-effect.tsx
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "../../utils/cn";
+
+export const TextGenerateEffect = ({
+  words,
+  className,
+}: {
+  words: string;
+  className?: string;
+}) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray: Array<string> = words.split(" ");
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+      },
+      {
+        duration: 2,
+        delay: stagger(0.35),
+      },
+    );
+  }, [scope.current]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span
+              key={word + idx}
+              className={
+                word === "Dinesh"
+                  ? "dark:text-slate-300 name cursor-default font-semibold text-transparent bg-clip-text bg-gradient-to-br from-[ee7752] to-[#e73c7e] text-black opacity-0"
+                  : "dark:text-slate-300 text-black opacity-0"
+              }
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div className={cn("font-normal", className)}>
+      <div className="mt-4">
+        <div className=" dark:text-slate-300 text-black text-4xl leading-snug tracking-wide font-sans">
+          {renderWords()}
+        </div>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,tsx}", "index.html"],
-  darkMode: 'class',
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./components/ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         sans: "'DM Sans'",
-      }
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [addVariablesForColors],
+};
 
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
