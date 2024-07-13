9a83a9bcc16a727ef8adcfad723b89d5f0d12db8
thatcatfromspace <encyclopedia748@gmail.com>
2024-07-13 17:19:44 +0530

    add project cards

---
+++ src/project-card.tsx
@@ -0,0 +1,44 @@
+import { ArrowUpRight } from "lucide-react";
+
+export const ProjectCard = ({
+  projectName,
+  projectDesc,
+  projectFrameworks,
+  projectLink,
+}: {
+  projectName: string;
+  projectDesc: string;
+  projectFrameworks: Array<string>;
+  projectLink?: string;
+}) => {
+  return (
+    <div className="w-full cursor-pointer flex-col rounded-lg p-4 shadow-lg dark:text-slate-300">
+      <div className="group flex items-center justify-start">
+        <a href={projectLink}>
+          <h3 className="text-lg font-semibold">{projectName}</h3>
+        </a>
+        {projectLink ? (
+          <ArrowUpRight
+            stroke="grey"
+            height={20}
+            className="transition-transform group-hover:-translate-y-0.5 group-hover:translate-x-0.5"
+          />
+        ) : null}
+      </div>
+      <p className="text-muted-foreground break-words">{projectDesc}</p>
+      <div className="mt-3 flex justify-start gap-x-3">
+        {projectFrameworks.map((framework, index) => (
+          <div key={index}>
+            <img
+              src={framework}
+              alt={"framework" + framework.toString()}
+              height={30}
+              width={30}
+              key={framework}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/projects.ts
+++ src/projects.ts
@@ -2,12 +2,25 @@ export const projects: Array<{
   name: string;
   desc: string;
   frameworks: Array<string>;
+  link?: string;
 }> = [
   {
     name: "Mozart",
-    desc: "Tracks your music listening history with ease!",
+    desc: "Track your music listening history with ease!",
+    frameworks: [
+      "https://raw.githubusercontent.com/thatcatfromspace/portfolio-v2/main/src/assets/nextjs.svg",
+      "https://raw.githubusercontent.com/thatcatfromspace/portfolio-v2/main/src/assets/nestjs.svg",
+      "https://raw.githubusercontent.com/thatcatfromspace/portfolio-v2/main/src/assets/postgresql.svg",
+    ],
+  },
+  {
+    name: "ProLeap",
+    desc: "Comprehensive app to manage mentor sessions.",
     frameworks: [
       "https://raw.githubusercontent.com/thatcatfromspace/portfolio-v2/main/src/assets/react.svg",
+      "https://raw.githubusercontent.com/thatcatfromspace/portfolio-v2/main/src/assets/django.svg",
+      "https://raw.githubusercontent.com/thatcatfromspace/portfolio-v2/main/src/assets/postgresql.svg",
     ],
+    link: "https://proleap.vercel.app",
   },
 ];
